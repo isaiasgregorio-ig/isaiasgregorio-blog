ce7e910864306cdc863d4b5ffb792fd47de4e6ee
isaiasgregorio-ig <isaias.gregorio@outlook.com>
2026-06-19 17:08:40 +0000

    Initial blog commit

---
+++ build-time/frontmatter.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+
+// Read markdown files and extract frontmatter
+export default function metaResolver(path) {
+	const file = fs.readFileSync(path, 'utf8');
+	const meta = matter(file);
+	return meta.data;
+}
